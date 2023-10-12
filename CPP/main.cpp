26ec064a218f0b10897ada6ad4836100ea15ce9b
trifoil <unixsslayer@gmail.com>
2023-10-12 22:19:46 +0200

    tasklist

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,48 +1,12 @@
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_ttf.h>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-int main(int argc, char* argv[]) {
-    SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
+int main(int argc, char *argv[]) {
 
-    // Create an SDL window and renderer
-    SDL_Window* window = SDL_CreateWindow("Text Rendering", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    // Load a specific font
-    TTF_Font* font = TTF_OpenFont("path_to_your_font.ttf", 24);
-
-    // Create a text surface
-    SDL_Color textColor = {0, 255, 255};
-    SDL_Surface* textSurface = TTF_RenderText_Solid(font, "Hello, SDL!", textColor);
-
-    // Create a texture from the text surface
-    SDL_Texture* textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-
-    // Clear the renderer and render the text
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, textTexture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-
-    // Main loop
-    bool quit = false;
-    SDL_Event event;
-    while (!quit) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-    }
-
-    // Cleanup
-    SDL_DestroyTexture(textTexture);
-    SDL_FreeSurface(textSurface);
-    TTF_CloseFont(font);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    TTF_Quit();
-    SDL_Quit();
-
-    return 0;
+if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    SDL_Log("SDL could not initialize: %s", SDL_GetError());
+    return 1;
+  }
 }
