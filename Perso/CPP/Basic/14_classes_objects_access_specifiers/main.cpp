63e3a1f2897692a9c78140a5d47aa624d516a22c
trifoil <unixsslayer@gmail.com>
2023-10-17 00:00:48 +0200

    a

--- Perso/CPP/Basic/14_classes_objects_access_specifiers/main.cpp
+++ Perso/CPP/Basic/14_classes_objects_access_specifiers/main.cpp
@@ -1,11 +1,26 @@
+#include <codecvt>
 #include <iostream>
+#include <string>
+#include <type_traits>
 
 using namespace std;
 
-class User {};
+class User {
+public:
+  void outputHelloworld() { cout << "hello world\n"; }
+
+  void setUserName(string name) { userName = name; }
+  string getUserName() { return userName; }
+
+private:
+  string userName;
+};
 
 int main() {
 
-  cout << "hello world\n";
+  User userExample;
+  userExample.setUserName("gugu");
+  cout << userExample.getUserName();
+  // userExample.outputHelloworld();
   return 0;
 }
--- Perso/CPP/Basic/1_hello_world/main.cpp
+++ Perso/CPP/Basic/1_hello_world/main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
+
   cout << "hello world\n";
+  
   return 0;
 }
