5c6913b67e2a33cd741d156146f0d2d79b9f764f
trifoil <unixsslayer@gmail.com>
2023-10-17 00:43:39 +0200

    a

---
+++ Perso/CPP/Basic/16_separate_files_and_constructors/User.cpp
@@ -0,0 +1,9 @@
+#include "User.h"
+#include <iostream>
+
+using namespace std;
+
+User::User()
+{
+cout << "Hello world\n";
+}
---
+++ Perso/CPP/Basic/16_separate_files_and_constructors/main.cpp
@@ -0,0 +1,10 @@
+#include "User.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    User user1;
+    return 0;
+}
