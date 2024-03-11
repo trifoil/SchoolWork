04079d1af5daec3516fe03fbbad1c8d09b380cf2
Florent Urbain <florenturbain@protonmail.com>
2024-03-11 15:54:21 +0100

    Suite exos chapitre 3

---
+++ bloc_1/quad_2/prog-tp/exos/3. L'héritage/TP6.py
@@ -0,0 +1,43 @@
+from TP1 import Humain
+
+class Sortilege:
+    def __init__(self, formule, effet, impardonnable=False):
+        self.__formule = formule
+        self.__effet = effet
+        self.__impardonnable = impardonnable
+
+    def SeLancer(self):
+        print(f"Le sort '{self.formule}' a été lancé avec succès.")
+
+    def Description(self):
+        print(f"Formule : {self.formule}\nEffet : {self.effet}")
+
+    @property
+    def formule(self):
+        return self.__formule
+
+
+class Sorcier(Humain):
+    def __init__(self, nom="", prenom="", annéeNaissance=0, baguette="", maison=""):
+        super().__init__(nom, prenom, annéeNaissance)
+        self.__baguette = baguette
+        self.__maison = maison
+        self.__sortilegesConnus = []
+
+
+    def LancerSort(self, sortilege):
+        if sortilege in self.__sortilegesConnus:
+            print("Lancement du sort :", sortilege.formule)
+            sortilege.SeLancer()
+        else:
+            raise ValueError("Le sort n'est pas connu")
+
+
+    def AjouterSort(self, sortilege):
+        self.__sortilegesConnus.append(sortilege)
+
+sortilege1 = Sortilege("Expelliarmus", "Désarme l'adversaire")
+sorcier1 = Sorcier("Harry", "Potter", 1980, "Épicéa, plume de phénix", "Gryffondor")
+
+sorcier1.AjouterSort(sortilege1)
+sorcier1.LancerSort(sortilege1)
