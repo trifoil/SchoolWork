e5c3e0e675579436c998017e177faabe0404ea77
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2024-09-01 23:07:59 +0200

    Update script.sh

--- Notes/2Bac/commun/quad_1/linux/TP/script.sh
+++ Notes/2Bac/commun/quad_1/linux/TP/script.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+
+sudo useradd user1
+sudo useradd user2
+sudo useradd examuser
+sudo useradd lastuser
+
+echo "user1:user1" | sudo chpassswd
+echo "user2:user2" | sudo chpasswd
+echo "examuser:examuser" | sudo chpasswd
+echo "lastuser:lastuser" | sudo chpasswd
+
+sudo chage -d 0 lastuser
 
